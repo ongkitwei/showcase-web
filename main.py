c5f62a55bea1dc7220a5508af42acffd69a2c6db
KIT WEI <ongkitwei@gmail.com>
2023-08-22 22:42:49 +0800

    2 columns, add title

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import pandas
+
+with st.sidebar:
+    st.write("Home")
+    st.write("Contact me")
 
 col1, col2 = st.columns(2)
 
@@ -7,3 +12,28 @@ with col1:
 
 with col2:
     st.title("Ong Kit Wei")
+    content =  """
+            Hi, i am kit wei a graduate EEE student from Singapore Polytechnic (SP).
+            My interest lies in robotics and i spent my 6 months internship in a Autonomus 
+            Vehicle(AV) company working as a Test Engineer.
+                """
+    st.info(content)
+
+content2 = """
+            Below you can find some of the apps i have built in Python. Feel free to contact me!
+            """    
+st.write(content2)
+
+
+col3, col4 = st.columns(2)
+
+
+data = pandas.read_csv("data.csv",sep=";")
+for index, row in data[:10].iterrows():
+    with col3:
+        st.title(row["title"])
+
+for index, row in data[10:].iterrows():
+    with col4:
+        st.title(row["title"])
+
